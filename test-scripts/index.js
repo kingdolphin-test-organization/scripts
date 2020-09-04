4fcabda51c8bf713cf6274949c6522d7e6881d4a
Leon Montealegre <leonm99@gmail.com>
2020-09-04 17:46:19 -0400

    Added test cli

--- test-scripts/index.js
+++ test-scripts/index.js
@@ -1,12 +1,43 @@
+#!/usr/bin/env node
+
+const jest = require("jest");
+const fs = require("fs");
 
 if (process.argv.length < 3)
     throw new Error("Invalid number of parameters!");
 
 const cmd = process.argv[2];
+const dir = process.cwd();
 
 switch (cmd) {
     case "build":
-        console.log(process.cwd());
+
+        break;
+    case "test":
+        let maps = {};
+
+        const tsconfig = JSON.parse(fs.readFileSync(`${dir}/tsconfig.json`) || "{}");
+        if (tsconfig["compilerOptions"] && tsconfig["compilerOptions"]["paths"]) {
+            const paths = tsconfig["compilerOptions"]["paths"];
+            Object.entries(paths).forEach(([name, [path]]) => {
+                name = name.replace("*", "(.*)$");
+                path = path.replace("*", "$1");
+                maps[name] = `<rootDir>/${path}.ts`;
+            });
+        }
+
+        jest.runCLI({
+            collectCoverage: false,
+            testEnvironment: "jsdom",
+            setupFiles: ["core-js"],
+            moduleFileExtensions: ["ts", "js"],
+            transform: {
+                "\\.(ts)$": "ts-jest"
+            },
+            testRegex: "/tests/.*\\.(test.ts)$",
+            moduleNameMapper: maps
+        });
+
         break;
     default:
         throw new Error(`Unknown command ${cmd}!`);
