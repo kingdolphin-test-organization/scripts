53dfd6bcb441858b4ef3f37126e5eea469f1ffb7
Leon Montealegre <leonm99@gmail.com>
2020-09-11 22:35:52 -0400

    Added start script

--- test-scripts/index.js
+++ test-scripts/index.js
@@ -2,7 +2,8 @@
 
 const fs = require("fs");
 const {run} = require("jest-cli");
-const {exec} = require("child_process");
+const {exec, execSync} = require("child_process");
+// const
 
 if (process.argv.length < 3)
     throw new Error("Invalid number of parameters!");
@@ -10,14 +11,39 @@ if (process.argv.length < 3)
 const cmd = process.argv[2];
 const dir = process.cwd();
 
+console.log(`COMMAND ${cmd}`);
+
 switch (cmd) {
     case "start":
         // look for and launch backend server
         // launch webpack dev server
         // watch code to rebuild
+        console.log("START");
+        // execSync("npm run test");
+        // execSync("npx react-scripts start");
+        execSync("node_modules/.bin/react-scripts start");
+
+        console.log("DONE EXEC??");
         break;
     case "build":
         // prod build
+        exec("npx react-scripts build");
+        break;
+    case "create":
+        // // create default empty project
+        // if (process.argv.length < 4)
+        //     throw new Error("Invalid number of parameters! Please specify a directory!");
+        // const dir = process.argv[3];
+        // exec(`mkdir ${dir}`)
+        // exec("npm init -y");
+        break;
+    case "lib":
+        // add opencircuits library submodule
+        if (process.argv.length < 4)
+            throw new Error("Invalid number of parameters! Need to know what library to add!");
+        const lib = process.argv[3];
+        const dir = (process.argv[4] || "lib")
+        exec(`git submodule add https://github.com/kingdolphin-test-organization/${lib} ${dir}/${lib}`);
         break;
     case "update":
         // pull and update submodules
