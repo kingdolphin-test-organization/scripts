1ac4b8f25a81185943e4fecbcb2b5a947f59daa7
Leon Montealegre <leonm99@gmail.com>
2020-09-14 23:27:01 -0400

    Updated

--- test-scripts/index.js
+++ test-scripts/index.js
@@ -2,80 +2,91 @@
 
 const fs = require("fs");
 const {run} = require("jest-cli");
-const {exec, execSync} = require("child_process");
-// const
+const {exec, spawn} = require("child_process");
 
 if (process.argv.length < 3)
     throw new Error("Invalid number of parameters!");
 
 const cmd = process.argv[2];
-const dir = process.cwd();
+const cwd = process.cwd();
 
-console.log(`COMMAND ${cmd}`);
+const cmds = new Map([
+    ["start", start],
+    ["build", build],
+    ["create", create],
+    ["lib", lib],
+    ["update", update],
+    ["test", test]
+]);
 
-switch (cmd) {
-    case "start":
-        // look for and launch backend server
-        // launch webpack dev server
-        // watch code to rebuild
-        console.log("START");
-        // execSync("npm run test");
-        // execSync("npx react-scripts start");
-        execSync("node_modules/.bin/react-scripts start");
 
-        console.log("DONE EXEC??");
-        break;
-    case "build":
-        // prod build
-        exec("npx react-scripts build");
-        break;
-    case "create":
-        // // create default empty project
-        // if (process.argv.length < 4)
-        //     throw new Error("Invalid number of parameters! Please specify a directory!");
-        // const dir = process.argv[3];
-        // exec(`mkdir ${dir}`)
-        // exec("npm init -y");
-        break;
-    case "lib":
-        // add opencircuits library submodule
-        if (process.argv.length < 4)
-            throw new Error("Invalid number of parameters! Need to know what library to add!");
-        const lib = process.argv[3];
-        const dir = (process.argv[4] || "lib")
-        exec(`git submodule add https://github.com/kingdolphin-test-organization/${lib} ${dir}/${lib}`);
-        break;
-    case "update":
-        // pull and update submodules
-        exec("git pull");
-        exec("git submodule foreach git pull origin master");
-        break;
-    case "test":
-        let maps = {};
+if (!cmds.has(cmd))
+    throw new Error(`Unknown command ${cmd}!`);
 
-        const tsconfig = JSON.parse(fs.readFileSync(`${dir}/tsconfig.json`) || "{}");
-        if (tsconfig["compilerOptions"] && tsconfig["compilerOptions"]["paths"]) {
-            const paths = tsconfig["compilerOptions"]["paths"];
-            Object.entries(paths).forEach(([name, [path]]) => {
-                name = name.replace("*", "(.*)$");
-                path = path.replace("*", "$1");
-                maps[name] = `<rootDir>/${path}.ts`;
-            });
-        }
+(cmds.get(cmd))(process.argv.slice(3));
 
-        run([
-            "--collectCoverage", "false",
-            "--testEnvironment", "jsdom",
-            "--setupFiles", "core-js",
-            "--moduleFileExtensions", "ts",
-            "--moduleFileExtensions", "js",
-            "--roots", "<rootDir>",
-            "--transform", `{"^.+\\\\.[jt]sx?$":"ts-jest"}`,
-            "--testRegex", `.*/tests/.*\\.(test.ts)$`,
-            "--moduleNameMapper", JSON.stringify(maps)
-        ], dir);
 
-        break;
-    default:
-        throw new Error(`Unknown command ${cmd}!`);
+function start() {
+    // look for and launch backend server
+    // launch webpack dev server
+    // watch code to rebuild
+    spawn("node_modules/.bin/react-scripts", ["start"], { stdio: "inherit" });
+}
+
+function build() {
+    // prod build
+    spawn("node_modules/.bin/react-scripts", ["build"], { stdio: "inherit" });
+}
+
+function create() {
+    // // create default empty project
+    // if (process.argv.length < 4)
+    //     throw new Error("Invalid number of parameters! Please specify a directory!");
+    // const dir = process.argv[3];
+    // exec(`mkdir ${dir}`)
+    // exec("npm init -y");
+
+}
+
+function lib(args) {
+    // add opencircuits library submodule
+    if (args.length < 1)
+        throw new Error("Invalid number of parameters! Need to know what library to add!");
+    const lib = args[0];
+    const dir = (args[1] || "lib")
+    exec(`git submodule add https://github.com/kingdolphin-test-organization/${lib} ${dir}/${lib}`);
+
+}
+
+function update() {
+    // pull and update submodules
+    exec("git pull");
+    exec("git submodule foreach git pull origin master");
+}
+
+function test() {
+    let maps = {};
+
+    const config = (fs.readFileSync(`${cwd}/tsconfig.paths.json`) || fs.readFileSync(`${cwd}/tsconfig.json`) || "{}")
+    const tsconfig = JSON.parse(config || "{}");
+    if (tsconfig["compilerOptions"] && tsconfig["compilerOptions"]["paths"]) {
+        const paths = tsconfig["compilerOptions"]["paths"];
+        Object.entries(paths).forEach(([name, [path]]) => {
+            name = name.replace("*", "(.*)$");
+            path = path.replace("*", "$1");
+            maps[name] = `<rootDir>/${path}.ts`;
+        });
+    }
+
+    run([
+        "--collectCoverage", "false",
+        "--testEnvironment", "jsdom",
+        "--setupFiles", "core-js",
+        "--moduleFileExtensions", "ts",
+        "--moduleFileExtensions", "js",
+        "--roots", "<rootDir>",
+        "--transform", `{"^.+\\\\.[jt]sx?$":"ts-jest"}`,
+        "--testRegex", `.*/tests/.*\\.(test.ts)$`,
+        "--moduleNameMapper", JSON.stringify(maps)
+    ], cwd);
 }
